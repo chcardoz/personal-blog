c6de9be4553de4818de14366c9fc10e8bbab3ded
Chris Cardoza <chris.siya511@gmail.com>
2023-01-04 02:22:48 +0400

    tailwind added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/settings">Settings</a>
+    <a href="/blog">Blog</a>
+  </nav>
+
+  <script>
+    import "../app.css";
+  </script>
+  
+  <slot></slot>
